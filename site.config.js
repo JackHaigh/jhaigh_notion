a39e59b10c85022f945c520d7ea07c414af69e52
Jack Haigh <jack.haigh@icloud.com>
2021-04-28 16:06:33 +0100

    Post-NPM Build

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '0ff49f6aa90c49bc8f9823f02fd37b05',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jack Haigh - Help Center',
+  domain: 'help.jackhaigh.com',
+  author: 'Jack Haigh',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'A Notion test!',
+  socialImageTitle: 'Help Center',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'jack_haigh',
+  github: 'JackHaigh',
+  linkedin: 'jackhaigh',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
